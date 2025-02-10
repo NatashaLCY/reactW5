07ae3368e9209d597f2553f8a508455cbc3b7c82
NatashaLCY <natashalinhy@gmail.com>
2025-02-10 22:22:04 +0800

    新增移除更新

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ const API_PATH = import.meta.env.VITE_API_PATH;
 function App() {
 	const [products, setProducts] = useState([]);
 	const [tempProduct, setTempProduct] = useState([]);
+	const [cart, setCart] = useState({});
+
+	const getCart = async()=>{
+		try {
+			const res = await axios.get(`${BASE_URL}/v2/api/${API_PATH}/cart`);
+			setCart(res.data.data);
+			//data axios,data api
+		} catch (error) {
+			alert('取得購物車失敗')
+		}
+	}
 
 	useEffect(() => {
 		const getProducts = async () => {
@@ -20,6 +31,7 @@ function App() {
 			}
 		};
 		getProducts();
+		getCart();
 	}, []);
 
 	const productModalRef = useRef(null);
@@ -44,18 +56,49 @@ function App() {
 
 	const [qtySelect, setQtySelect] = useState(1);
 
-  const addCartItem = async(product_id, qty) =>{
-    try {
-      await axios.post(`${BASE_URL}/v2/api/${API_PATH}/cart`,{
-        data:{
-          product_id,
-          qty:Number(qty)
-        }
-      })
-    } catch (error) {
-      alert('加入購物車失敗')
-    }
-  }
+	const addCartItem = async(product_id, qty) =>{
+		try {
+			await axios.post(`${BASE_URL}/v2/api/${API_PATH}/cart`,{
+				data:{
+					product_id,
+					qty:Number(qty)
+				}
+			})
+			getCart();
+		} catch (error) {
+			alert('加入購物車失敗')
+		}
+	}
+	const removeCartItem = async (cartItem_id) => {
+		try {
+			await axios.delete(`${BASE_URL}/v2/api/${API_PATH}/cart/${cartItem_id}`);
+			getCart();
+		} catch (error) {
+			alert("移除商品失敗");
+		}
+	};
+	const removeCart = async() =>{
+		try {
+			await axios.delete(`${BASE_URL}/v2/api/${API_PATH}/carts`)
+			getCart();
+		} catch (error) {
+			alert('移除購物車失敗')
+		}
+	}
+	const updateCartItem = async (cartItem_id, product_id, qty) => {
+		try {
+			await axios.put(`${BASE_URL}/v2/api/${API_PATH}/cart/${cartItem_id}`, {
+				data:{
+					product_id,
+					qty:Number(qty)
+				}
+			});
+			getCart();
+		} catch (error) {
+			alert("更新商品失敗");
+		}
+	};
+
 
 	return (
 		<div className="container">
@@ -128,57 +171,66 @@ function App() {
 						</div>
 					</div>
 				</div>
+				{cart.carts?.length > 0 && (
+					<div>
+						<div className="text-end py-3">
+							<button onClick={removeCart} className="btn btn-outline-danger" type="button">
+								清空購物車
+							</button>
+						</div>
 
-				<div className="text-end py-3">
-					<button className="btn btn-outline-danger" type="button">
-						清空購物車
-					</button>
-				</div>
+						<table className="table align-middle">
+							<thead>
+								<tr>
+									<th></th>
+									<th>品名</th>
+									<th style={{ width: "150px" }}>數量/單位</th>
+									<th className="text-end">單價</th>
+								</tr>
+							</thead>
 
-				<table className="table align-middle">
-					<thead>
-						<tr>
-							<th></th>
-							<th>品名</th>
-							<th style={{ width: "150px" }}>數量/單位</th>
-							<th className="text-end">單價</th>
-						</tr>
-					</thead>
-
-					<tbody>
-						<tr>
-							<td>
-								<button type="button" className="btn btn-outline-danger btn-sm">
-									x
-								</button>
-							</td>
-							<td></td>
-							<td style={{ width: "150px" }}>
-								<div className="d-flex align-items-center">
-									<div className="btn-group me-2" role="group">
-										<button type="button" className="btn btn-outline-dark btn-sm">
-											-
-										</button>
-										<span className="btn border border-dark" style={{ width: "50px", cursor: "auto" }}></span>
-										<button type="button" className="btn btn-outline-dark btn-sm">
-											+
-										</button>
-									</div>
-									<span className="input-group-text bg-transparent border-0">unit</span>
-								</div>
-							</td>
-							<td className="text-end">單項總價</td>
-						</tr>
-					</tbody>
-					<tfoot>
-						<tr>
-							<td colSpan="3" className="text-end">
-								總計：
-							</td>
-							<td className="text-end" style={{ width: "130px" }}></td>
-						</tr>
-					</tfoot>
-				</table>
+							<tbody>
+								{cart.carts?.map((cartItem) => (
+									<tr key={cartItem.id}>
+										<td>
+											<button onClick={() => removeCartItem(cartItem.id)} type="button" className="btn btn-outline-danger btn-sm">
+												x
+											</button>
+										</td>
+										<td>{cartItem.product.title}</td>
+										<td style={{ width: "150px" }}>
+											<div className="d-flex align-items-center">
+												<div className="btn-group me-2" role="group">
+													<button onClick={() => updateCartItem(cartItem.id, cartItem.product.id, cartItem.qty - 1)} type="button" className="btn btn-outline-dark btn-sm">
+														-
+													</button>
+													<span className="btn border border-dark" style={{ width: "50px", cursor: "auto" }}>
+														{cartItem.qty}
+													</span>
+													<button onClick={() => updateCartItem(cartItem.id, cartItem.product.id, cartItem.qty + 1)} type="button" className="btn btn-outline-dark btn-sm">
+														+
+													</button>
+												</div>
+												<span className="input-group-text bg-transparent border-0">{cartItem.product.unit}</span>
+											</div>
+										</td>
+										<td className="text-end">{cartItem.total}</td>
+									</tr>
+								))}
+							</tbody>
+							<tfoot>
+								<tr>
+									<td colSpan="3" className="text-end">
+										總計：
+									</td>
+									<td className="text-end" style={{ width: "130px" }}>
+										{cart.total}
+									</td>
+								</tr>
+							</tfoot>
+						</table>
+					</div>
+				)}
 			</div>
 
 			<div className="my-5 row justify-content-center">
